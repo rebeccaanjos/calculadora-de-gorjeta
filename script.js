805319efa14c1f9fce91df21eb2adeff7a5878e4
rebecca.anjos30@gmail.com <rebecca.anjos30@gmail.com>
2024-03-23 18:43:47 -0300

    sétimo commit do HTML e primeiro commit do JS

---
+++ script.js
@@ -0,0 +1,27 @@
+let conta = 0
+let pessoas = 0
+
+const contaInput = document.querySelector("#conta")
+contaInput.addEventListener("input", receberValorConta)
+
+function receberValorConta(event) {
+     conta = Number(event.target.value)
+}
+
+const pessoasInput = document.querySelector("#pessoas")
+pessoasInput.addEventListener("input", receberQuantidadePessoas)
+
+function receberQuantidadePessoas(event) {
+    const paragrafoErro = document.querySelector(".pessoas #erro")
+    const divErro = document.querySelector(".pessoas .input-box")
+
+    if(event.target.value === "0") {    
+        paragrafoErro.styles.display = "block"
+        divErro.setAttribute("id", "erro-div")
+    } else {
+        paragrafoErro.styles.display = "none"
+        divErro.setAttribute("id", "")
+        pessoas = Number(event.target.value)
+    }
+
+}
