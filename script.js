07f87005868694d8fb54d971b2e023272a73f6c7
rebecca.anjos30@gmail.com <rebecca.anjos30@gmail.com>
2024-03-23 18:55:20 -0300

    segundo commit do JS

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let conta = 0
 let pessoas = 0
+let porcentagem = 0 
 
 const contaInput = document.querySelector("#conta")
 contaInput.addEventListener("input", receberValorConta)
@@ -23,5 +24,13 @@ function receberQuantidadePessoas(event) {
         divErro.setAttribute("id", "")
         pessoas = Number(event.target.value)
     }
+}
 
+const botoesGorjeta = document.querySelectorAll(".gorjeta input[type='button']")
+botoesGorjeta.forEach(botao=>{
+    botao.addEventListener("click",)
+})
+
+function receberPorcentagem(event) {
+    
 }
